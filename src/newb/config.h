5c5e22de99fcf82e103d02b6551e58cc317ee2a4
ThaiMCYTB <thaimc437@gmail.com>
2024-08-03 12:58:28 +0700

    Update config.h

--- src/newb/config.h
+++ src/newb/config.h
@@ -32,7 +32,7 @@
 /* Color correction */
 #define NL_TONEMAP_TYPE 4   // 1:Exponential, 2:Reinhard, 3:Extended Reinhard, 4:ACES
 #define NL_CONSTRAST 1.8   // 0.3 low ~ 2.0 high
-#define NL_EXPOSURE 1.7   // [toggle] 0.5 dark ~ 3.0 bright
+#define NL_EXPOSURE 2.1   // [toggle] 0.5 dark ~ 3.0 bright
 #define NL_SATURATION 1.5 // [toggle] 0.0 grayscale ~ 4.0 super saturated
 //#define NL_TINT vec3(1.0,0.75,0.5) // [toggle] color overlay
 
